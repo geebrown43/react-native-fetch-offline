93aaefd1661159cbf4e2db7cb73aa1f99db1c840
Hardeep Singh <hardeep_singh@jbhunt.com>
2019-04-05 01:30:17 -0500

    added option to ignore path for key for cache

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import { AsyncStorage } from 'react-native';
 import hash from 'object-hash';
+import _ from 'lodash';
 import omit from 'lodash/omit';
 import Network from 'react-native-internet-reachability';
 
-export default async function FetchOffline(url, options) {
+export default async function FetchOffline(url, options, ignoredRoutesForCacheKey) {
   const isReachable = await Network.isReachable();
   if (isReachable) {
     return fetch(url, options);
   } else {
-    return getDataFromCache(url, options);
+    return getDataFromCache(url, options, ignoredRoutesForCacheKey);
   }
 }
 
-async function getDataFromCache(url, options) {
-  const data = await getFailCaseResponse(url, options);
+async function getDataFromCache(url, options, ignoredRoutesForCacheKey) {
+  const data = await getFailCaseResponse(url, options, ignoredRoutesForCacheKey);
   if (data) {
     return new Promise((resolve) => {
       resolve({
@@ -26,8 +27,8 @@ async function getDataFromCache(url, options) {
   }
   return Promise.reject(new Error('No Internet'));
 }
- export async function getFailCaseResponse(url, options) {
-  const key = genrateKeyFor(url, options);
+ export async function getFailCaseResponse(url, options, ignoredRoutesForCacheKey) {
+  const key = genrateKeyFor(url, options, ignoredRoutesForCacheKey);
   let data = await AsyncStorage.getItem(key);
   if (data) {
     data = JSON.parse(data);
@@ -48,8 +49,8 @@ export function isResponseFromOnline(result){
   }
 
 }
-export function cacheReponse(url, options, responseBody) {
-  const key = genrateKeyFor(url, options);
+export function cacheReponse(url, options, responseBody, ignoredRoutesForCacheKey) {
+  const key = genrateKeyFor(url, options, ignoredRoutesForCacheKey);
   AsyncStorage.setItem(key, JSON.stringify(responseBody));
 }
 
@@ -60,8 +61,10 @@ function getDefaultResponseFromOptions(options) {
   return null;
 }
 
-function genrateKeyFor(url, options) {
+function genrateKeyFor(url, options, ignoredRoutesForCacheKey) {
   const providedOtions = omit(options, ['offline']);
+  for (var path in ignoredRoutesForCacheKey) {
+    _.set(providedOtions, path, '');
+  }
   return `options:${hash(providedOtions)} url:${url}}`;
 }
-
